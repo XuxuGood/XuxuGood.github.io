7c0a6adcc587e0d6bd7811603ae3c0fea282349c
xuxu <22476705@qq.com>
2020-01-18 14:09:52 +0000

    TravisCI 自动部署

---
+++ js/src/clipboard-use.js
@@ -0,0 +1,3 @@
+// build time:Sat Jan 18 2020 14:09:08 GMT+0000 (Coordinated Universal Time)
+!function(n,t,a){var i=function(){var n="";n+='<button class="btn-copy" data-clipboard-snippet="">';n+='  <i class="fa fa-clipboard"></i><span>复制</span>';n+="</button>";$(".highlight .code pre").before(n);new ClipboardJS(".btn-copy",{target:function(n){return n.nextElementSibling}})};i()}(window,document);
+//rebuild by neat 
