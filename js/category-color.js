afafa9d9ac2a0ea9b2dda5a2fc12a1e2280c06d3
xuxu <22476705@qq.com>
2020-05-09 08:52:41 +0000

    TravisCI

---
+++ js/category-color.js
@@ -0,0 +1,19 @@
+// pjax只有进入分类页面才加载js
+if (window.location.href.indexOf("/categories/") !== -1) {
+    var allCategory = document.getElementsByClassName('category-all');
+    if (allCategory.length > 0) {
+        //链接
+        var categories = allCategory[0].getElementsByTagName('a');
+        //数字
+        var counts = allCategory[0].getElementsByTagName('span');
+        for (var i = categories.length - 1; i >= 0; i--) {
+            var r = Math.floor(Math.random() * 75 + 130);
+            var g = Math.floor(Math.random() * 75 + 100);
+            var b = Math.floor(Math.random() * 75 + 80);
+            //链接颜色
+            categories[i].style.background = "rgb(" + r + "," + g + "," + b + ")";
+            //数字颜色
+            counts[i].style.color = "rgb(" + r + "," + g + "," + b + ")";
+        }
+    }
+}
