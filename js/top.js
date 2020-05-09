c2610adac7497a4cc7b82b064c640a98533ad43d
xuxu <22476705@qq.com>
2020-05-09 08:57:57 +0000

    TravisCI

---
+++ js/top.js
@@ -0,0 +1,23 @@
+// pjax只有进入top页面才加载js
+if (window.location.href.indexOf("/top/") !== -1){
+    var time = 0
+    var title = ""
+    var url = ""
+    var query = new AV.Query('Counter');
+    query.notEqualTo('id', 0);
+    query.descending('time');
+    query.limit(1000);
+    query.find().then(function (todo) {
+        for (var i = 0; i < 1000; i++) {
+            var result = todo[i].attributes;
+            time = result.time;
+            title = result.title;
+            url = result.url;
+            var content = "<p class='my-article-top'>" + "<font color='#a7a7e5'>" + "➤【热度: " + "</font>" + "<font color='#f1a8ce'>" + time + " ℃】" + "</font>" + "<a href='" + url + "'>" + title + "</a>" + "</p>";
+            document.getElementById("top").innerHTML += content
+        }
+    }, function (error) {
+        console.log(error);
+    });
+}
+
