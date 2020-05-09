00cbcc75119a82e20586341d0dce79618128c029
xuxu <22476705@qq.com>
2020-05-09 09:32:38 +0000

    TravisCI

---
+++ js/links-color.js
@@ -0,0 +1,19 @@
+// pjax只有进入友链页面才加载js
+if (window.location.href.indexOf("/links/") !== -1) {
+    $(document).ready(function () {
+        let nickname, avatar, site, info, li = "";
+        $.getJSON("/links/linklist.json",
+            function (data) {
+                for (let i = 0; i < data.length; i++) {
+                    nickname = data[i].nickname;
+                    avatar = data[i].avatar;
+                    site = data[i].site;
+                    info = data[i].info;
+                    li += '<li class="card">' + '<a href="' + site + '" title="' + info + '" target="_blank">' +
+                        '<div class="thumb" style="background-image: url(' + avatar + ');"> ' + '</div>' + '<span class="sitename">' + nickname + '</span>' +
+                        '<div class="linkdes">' + info + '</div> ' + '</a>' + '</li>';
+                }
+                $("#link-navigation").append(li);
+            });
+    });
+}
