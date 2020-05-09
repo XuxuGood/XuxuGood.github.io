e597a73a3696b3f2c4a8801208824a0ae057ae55
xuxu <22476705@qq.com>
2020-05-09 16:01:50 +0000

    TravisCI

---
+++ js/tag-color.js
@@ -0,0 +1,13 @@
+// pjax只有进入标签页面才加载js
+if (window.location.href.indexOf("/tags/") !== -1) {
+    var allTags = document.getElementsByClassName('tag-cloud-tags');
+    if (allTags.length > 0) {
+        var tags = allTags[0].getElementsByTagName('a');
+        for (var i = tags.length - 1; i >= 0; i--) {
+            var r = Math.floor(Math.random() * 75 + 130);
+            var g = Math.floor(Math.random() * 75 + 100);
+            var b = Math.floor(Math.random() * 75 + 80);
+            tags[i].style.background = "rgb(" + r + "," + g + "," + b + ")";
+        }
+    }
+}
