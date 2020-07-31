bd08e1569ad1d6c9c63558462db31d2261d04981
xuxu <22476705@qq.com>
2020-07-31 15:38:00 +0000

    TravisCI

---
+++ js/aplayer.js
@@ -0,0 +1,33 @@
+// 播放/暂停
+function aplayerToggle() {
+    let ap = document.querySelector('meting-js').aplayer;
+    if (ap != undefined) {
+        ap.toggle();
+    }
+}
+
+// 上一曲
+function aplayerBackward() {
+    let ap = document.querySelector('meting-js').aplayer;
+    if (ap != undefined) {
+        ap.skipBack();
+        ap.play();
+    }
+}
+
+// 下一曲
+function aplayerForward() {
+    let ap = document.querySelector('meting-js').aplayer;
+    if (ap != undefined) {
+        ap.skipForward();
+        ap.play();
+    }
+}
+
+// 调节音量
+function aplayerVolume(percent) {
+    let ap = document.querySelector('meting-js').aplayer;
+    if (ap != undefined) {
+        ap.volume(percent);
+    }
+}
